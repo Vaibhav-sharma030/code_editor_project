49fd859799d35ccfe419b43b257526477b1f4054
Vaibhav-sharma030 <vaibhavsharmaoct@gmail.com>
2026-05-30 16:54:42 +0530

    03-authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import { Button } from "@/components/ui/button";
-import { db } from "@/lib/db";
-import Image from "next/image";
+import UserButton from "@/modules/auth/components/user-button";
 
 export default async function Home() {
-
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <Button>
-        Get Started 
+        Get Started
       </Button>
+      <UserButton></UserButton>
     </div>
   );
 }
